e0acc1bffbe7cd3223ce3e9b445f36eb25abac42
Anand9598 <anandtiwari4009@gmail.com>
2024-07-24 23:52:10 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,64 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-full bg-black h-[440px]">
+      <div className="flex flex-row text-white justify-evenly gap-[87px] pt-20 px-[134px] pb-[95px]">
+        <div className="flex flex-col gap-4 ">
+          <p className="tracking-wide text-lg font-bold font-inter w-[118px] h-[24px]">
+            Exclusive
+          </p>
+          <a className="font-medium font-poppin w-[101px] h-[28px] ">
+            Subscribe
+          </a>
+          <p className="font-extralight font-poppin">
+            Get 10% Off on your first order
+          </p>
+          <input
+            className="border-[1.5px] rounded-md bg-black font-poppin text-white font-normal pl-2 py-3 pr-4"
+            type="text"
+            placeholder="Enter Your email"
+          />
+        </div>
 
-export default Footer
+        <div className="flex flex-col gap-6 w-[175px] ">
+          <p>Support</p>
+
+          <p className="font-extralight">
+            111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.
+          </p>
+          <p className="font-extralight">exclusive@gmail.com</p>
+          <p className="font-extralight">+88015-88888-9999</p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <p className="font-poppin">Account</p>
+
+          <p className="font-extralight font-poppin">My Account</p>
+          <p className="font-extralight font-poppin">Login / Register</p>
+          <p className="font-extralight font-poppin">Cart</p>
+          <p className="font-extralight font-poppin">Wishlist</p>
+          <p className="font-extralight font-poppin">Shop</p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <p className="font-poppin">Quick Links</p>
+
+          <p className="font-poppin font-extralight">Privacy Policy</p>
+          <p className="font-extralight font-poppin">Terms Of Use</p>
+          <p className="font-extralight font-poppin">FAQ</p>
+          <p className="font-extralight font-poppin">Contact</p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <p className="font-poppin">Download App</p>
+        </div>
+      </div>
+      <div className="w-full bg-black text-gray-500 flex justify-center items-center py-3 border-gray-400 border-t-[0.5px]">
+        <span className="font-poppin">
+          &#9400; Copyright Rimel 2022. All right reserved{" "}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,56 @@
 const Login = () => {
   return (
     <div className="w-11/12 flex">
-     <div className="6/12">
-      <img src="/SideImage.png" alt="" className="w-[805px] h-[781px]"/>
-     </div>
-     <div className="w-4/12 flex items-center justify-end">
-    
-     <div>
-     <h1>Create an account</h1>
+      <div className="6/12">
+        <img
+          src="/SideImage.png"
+          alt=""
+          className="w-[805px] h-[781px] mt-[60px] mb-[140px]"
+        />
+      </div>
+      <div className="w-4/12 flex items-center py-[125px] justify-end">
+        <div className="flex flex-col gap-12">
+          <div>
+            <h1 className="w-[339px] tracking-widest text-3xl h-[30px] font-medium mb-6 ">
+              Create an account
+            </h1>
 
-     <h4>Enter your details below</h4>
-     
-     <form action="" className="flex flex-col">
-     <input type="text" placeholder="Name" />
-     <input type="text" placeholder= "Email or Phone Number" />
-     <input type="password" placeholder="Password" />
-     <button>Create Account</button>
-     <button>Sign up with Google</button>
-     <span>Already have account ? <a>Login</a></span>
-     </form>
-     </div>
-     </div>
+            <p className="text-sm font-medium">Enter your details below</p>
+          </div>
+          <form action="" className="flex flex-col  gap-10 mt-3">
+            <input
+              className="border-b-[1.5px] border-black"
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              className="border-b-[1.5px] border-black"
+              type="text"
+              placeholder="Email or Phone Number"
+            />
+            <input
+              className="border-b-[1.5px] border-black"
+              type="password"
+              placeholder="Password"
+            />
+
+            <button
+              className="bg-red-500 py-3 px-4 text-white rounded-md"
+              type="button"
+            >
+              Create Account
+            </button>
+            <button className="border-black border-2 py-3">
+              Sign up with Google
+            </button>
+            <span className="flex justify-center items-center">
+              Already have account ? <a>Login</a>
+            </span>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
